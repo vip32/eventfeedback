46839f929fee19163c5c071b9ac4fa0a7292ea13
Vincent van Proosdij <mini32@gmail.com>
2013-11-15 12:32:30 +0100

    testtrace

--- Web.Api/Controllers/Admin/UsersController.cs
+++ Web.Api/Controllers/Admin/UsersController.cs
@@ -33,6 +33,8 @@ namespace EventFeedback.Web.Api.Controllers.Admin
         public IEnumerable<UserAdminBindingModel> Get([FromUri] string filter = "")
         {
             //Thread.Sleep(1500);
+            Trace.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!! TEST TRACE");
+
             _traceSource.TraceInformation("usersscontroller get all");
             IEnumerable<User> users;
 
