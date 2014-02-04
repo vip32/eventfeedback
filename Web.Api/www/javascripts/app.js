1e2f18390c40b80e04ee8ba541e9da9c23c4fd0f
Vincent van Proosdij <mini32@gmail.com>
2014-02-04 22:54:49 +0100

    image fix

--- Web.Api/www/javascripts/app.js
+++ Web.Api/www/javascripts/app.js
@@ -1642,7 +1642,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
     + escapeExpression(((stack1 = ((stack1 = depth0.model),stack1 == null || stack1 === false ? stack1 : stack1.userName)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "&emsp;/&emsp;"
     + escapeExpression(((stack1 = ((stack1 = depth0.model),stack1 == null || stack1 === false ? stack1 : stack1.password)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</h3></div>\r\n        <div class=\"col-xs-6\"><img src=\"images/qr.png\" alt=\"qr\" width=\"88\" height=\"88\" /></div>\r\n        <div class=\"col-xs-6\"></div>\r\n    </div>\r\n    <a href=\"https://eventfeedback.azurewebsites.net\">https://eventfeedback.azurewebsites.net</a>\r\n</div>\r\n";
+    + "</h3></div>\r\n        <div class=\"col-xs-6\"><img src=\"../images/qr.png\" alt=\"qr\" width=\"88\" height=\"88\" /></div>\r\n        <div class=\"col-xs-6\"></div>\r\n    </div>\r\n    <a href=\"https://eventfeedback.azurewebsites.net\">https://eventfeedback.azurewebsites.net</a>\r\n</div>\r\n";
   return buffer;
   });
 if (typeof define === 'function' && define.amd) {
