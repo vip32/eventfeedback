60ce2a6acbf499ca1ef36f5933011135acd1c673
Vincent van Proosdij <mini32@gmail.com>
2014-03-27 17:07:14 +0100

    string

--- Web.Api/www/javascripts/app.js
+++ Web.Api/www/javascripts/app.js
@@ -491,6 +491,64 @@ Backbone.Marionette.Renderer.render = function(templateName, data) {
 };
 });
 
+;require.register("lib/string-helper", function(exports, require, module) {
+if (typeof String.prototype.startsWith !== 'function') {
+  String.prototype.startsWith = function(str) {
+    return this.slice(0, str.length) === str;
+  };
+}
+
+if (typeof String.prototype.endsWith !== 'function') {
+  String.prototype.endsWith = function(str) {
+    return this.slice(-str.length) === str;
+  };
+}
+
+if (typeof String.prototype.addCommas !== 'function') {
+  String.prototype.addCommas = function() {
+    var rgx, str, x, x1, x2;
+    str = this;
+    str += '';
+    x = str.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+      x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+  };
+}
+
+if (typeof String.prototype.lpad !== 'function') {
+  String.prototype.lpad = function(padString, length) {
+    var str;
+    str = this;
+    while (str.length < length) {
+      str = padString + str;
+    }
+    return str;
+  };
+}
+
+if (typeof String.prototype.rpad !== 'function') {
+  String.prototype.rpad = function(padString, length) {
+    var str;
+    str = this;
+    while (str.length < length) {
+      str = str + padString;
+    }
+    return str;
+  };
+}
+
+if (typeof String.prototype.trim !== 'function') {
+  String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g, '');
+  };
+}
+});
+
 ;require.register("lib/view-helper", function(exports, require, module) {
 Handlebars.registerHelper('pick', function(val, options) {
   return options.hash[val];
@@ -2454,8 +2512,7 @@ module.exports = SigninView = (function(_super) {
   };
 
   SigninView.prototype.onShow = function() {
-    scrollTo(0, 0);
-    return $('#make-switch').bootstrapSwitch('size', 'small');
+    return scrollTo(0, 0);
   };
 
   SigninView.prototype.onClose = function() {
---
+++ Web.App/app/lib/string-helper.coffee
@@ -0,0 +1,37 @@
+﻿if (typeof String::startsWith != 'function') 
+  String::startsWith = (str) ->
+    return this.slice(0, str.length) == str
+ 
+if (typeof String::endsWith != 'function')
+  String::endsWith = (str) ->
+    return this.slice(-str.length) == str
+ 
+if (typeof String::addCommas != 'function')
+  String::addCommas = ->
+    str = this
+    str += ''
+    x = str.split('.')
+    x1 = x[0]
+    x2 = if x.length > 1 then '.' + x[1] else ''
+    rgx = /(\d+)(\d{3})/
+    while (rgx.test(x1))
+      x1 = x1.replace(rgx, '$1' + ',' + '$2')
+    x1 + x2
+ 
+if (typeof String::lpad != 'function')
+  String::lpad = (padString, length) ->
+    str = this
+    while str.length < length
+      str = padString + str
+    return str
+ 
+if (typeof String::rpad != 'function')
+  String::rpad = (padString, length) ->
+    str = this
+    while str.length < length
+      str = str + padString
+    return str
+ 
+if (typeof String::trim != 'function')
+  String::trim = ->
+    this.replace(/^\s+|\s+$/g, '')
