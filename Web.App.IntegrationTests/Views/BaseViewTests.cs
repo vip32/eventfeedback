0ebf4a1eb4be72e840388139cac725d851f17a32
Vincent van Proosdij <mini32@gmail.com>
2014-04-04 00:54:39 +0200

    add basetest

---
+++ Web.App.IntegrationTests/Views/BaseViewTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Coypu;
+using Coypu.Drivers.Selenium;
+
+namespace Eventfeedback.Web.App.IntegrationTests.UI
+{
+    public class BaseViewTests
+    {
+        public Settings Settings = new Settings();
+        public Views Views = new Views();
+
+        public void Do(Action<BrowserSession> action)
+        {
+            using (var browser = new BrowserSession(new SessionConfiguration
+            {
+                Driver = typeof(SeleniumWebDriver),
+                Browser = Coypu.Drivers.Browser.Chrome,
+                AppHost = Settings.AppHost, Port = Settings.Port, SSL = Settings.SSL
+            }))
+            {
+                action(browser);
+            }
+        }
+    }
+}
