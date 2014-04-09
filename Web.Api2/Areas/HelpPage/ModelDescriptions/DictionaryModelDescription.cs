ec6ceb05177a25caabaf61fabe399d1707cbaf9d
Vincent van Proosdij <mini32@gmail.com>
2014-04-09 15:16:23 +0200

    new web.api with helppage added

---
+++ Web.Api2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Eventfeedback.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Web.Api2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eventfeedback.Web.Api.WebApiApplication" Language="C#" %>
