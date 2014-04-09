8b4572f7317c54825600000612bbd14d0caa43b8
Vincent van Proosdij <mini32@gmail.com>
2014-04-09 09:26:29 +0200

    before up[date

--- Chrome.App/background.js
+++ Chrome.App/background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.chrome.html', {
     'bounds': {
       'width': 640,
-      'height': 410
+      'height': 510
     }
   });
 });
