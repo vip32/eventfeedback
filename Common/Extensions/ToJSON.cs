835042d29750cc44baa737183c88d43e22d2d4d0
Vincent van Proosdij <mini32@gmail.com>
2014-04-09 16:00:21 +0200

    ToJSON

---
+++ Common/Extensions/ToJSON.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Script.Serialization;
+
+namespace EventFeedback.Common
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Serializes the object to JSON.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static string ToJSON(this object obj)
+        {
+            if (obj == null) return null;
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(obj);
+        }
+    }
+}
