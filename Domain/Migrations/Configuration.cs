7637984f916a1b2ac1175bc4f96be1249ed587bf
vip32 <32github32>
2015-02-10 22:00:13 +0100

    testdata seed disabled

--- Domain/Migrations/Configuration.cs
+++ Domain/Migrations/Configuration.cs
@@ -1,11 +1,8 @@
 namespace EventFeedback.Domain.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<EventFeedback.Domain.DataContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContext>
     {
         public Configuration()
         {
@@ -14,19 +11,7 @@ namespace EventFeedback.Domain.Migrations
 
         protected override void Seed(EventFeedback.Domain.DataContext context)
         {
-            TestData.Seed(context);
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            // TestData.Seed(context);
         }
     }
 }
