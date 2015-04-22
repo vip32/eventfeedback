f127791af638d5cb45653735e3df4e3276456150
vip32 <32github32>
2015-04-22 12:03:59 +0200

    www3

--- Web.Api/service-worker.js
+++ Web.Api/service-worker.js
@@ -1,5 +1,4 @@
 /*
-  ROOT COPY, ORIGINAL IN /WWW
   This service worker doesn't actually do anything!
   In the real world, you would add support for Offline or the ability to
   handle push messages.
