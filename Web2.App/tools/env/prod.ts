13832ab3d55933119f985d3046ff7a37edafbe96
vvproosdij <vvproosdij@NB-000674>
2016-10-06 18:11:24 +0200

    api root prod

--- Web2.App/tools/env/prod.ts
+++ Web2.App/tools/env/prod.ts
@@ -3,8 +3,6 @@ import { EnvConfig } from './env-config.interface';
 const ProdConfig: EnvConfig = {
   ENV: 'PROD',
   API: ''
-  // API: 'https://eventfeedback.azurewebsites.net'
-  //API: 'https://eventfeedback-staging.azurewebsites.net'
 };
 
 export = ProdConfig;
